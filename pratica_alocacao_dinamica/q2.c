a32667c6d99f01055033275a8d4187435da1f32c
CauehCraft <12563720+CauehCraft@users.noreply.github.com>
2023-02-23 21:16:46 -0300

    Terminado: ALOCAÇÃO DINÂMICA VETORES

---
+++ pratica_alocacao_dinamica/q2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define N_ALUNOS 10
+
+int main(){
+int i, ii, n_aprovados = 0;
+char resp;
+float n_questoes, nota = 0;
+
+//Pedindo para o usuario definir a quantidade de questoes da prova
+printf("Digite a quantidade de questoes da prova: ");
+scanf("%f", &n_questoes);
+
+//alocação dinamica e verificacao de alocacao bem sucedida
+float *provas = (float*) malloc(N_ALUNOS*sizeof(float));
+char *gabarito = (char*) malloc(n_questoes*sizeof(char));
+if(provas == NULL || gabarito == NULL){
+        printf("Erro de alocação de memoria\n");
+        exit(1);
+}
+ 
+//Pedindo para o usuario digitar o gabrito
+printf("Digite o gabarito da prova com somente letras.\nEx.: 'a' ou 'B'\n");
+for(i=0;i<n_questoes;i++){
+    printf("Questao %d: ", i+1);
+    scanf(" %c", &gabarito[i]);
+    gabarito[i] = toupper(gabarito[i]);
+}
+
+//Pedindo para o usuario digitar as respostas dos alunos
+for (i=0;i<N_ALUNOS;i++){
+    printf("Digite as respostas do aluno %d\n", i+1);
+    for (ii=0;ii<n_questoes;ii++){
+        printf("Questao %d: ", ii+1);
+        scanf(" %c", &resp);
+        resp = toupper(resp);
+        if(resp == gabarito[ii]){
+            nota += 10/n_questoes;
+        }
+    }
+    provas[i] = nota;
+    nota = 0;
+}
+//Informando a nota dos alunos ao usuario
+for (i=0;i<N_ALUNOS;i++){
+    printf("Aluno %d\nNota: %.2f\n", i+1, provas[i]);
+    if (provas[i]>=6){
+        n_aprovados++;
+    }
+}
+
+//Calculando e exibindo percentual de aprovacao
+//aprovacao = n_aprovados*100/N_ALUNOS
+printf("Foram aprovados %d%% dos alunos!", n_aprovados*100/N_ALUNOS);
+
+//Liberando memoria
+free(provas);
+free(gabarito);
+return 0;
+}
--- pratica_alocacao_dinamica/vetores/q1.c
+++ pratica_alocacao_dinamica/vetores/q1.c
@@ -1,6 +1,69 @@
-#include <std.io>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define N_ENTREVISTADOS 4
 
-int main(){
-    printf("Github test...");
+int main(void){
+    //Variaveis auxiliares
+    int i, op, n_mulher=0, n_homem=0;
+    float perc_f=0;
+    float perc_m=0;
+
+    //vetores dinamicos 
+    char *sexo = (char*) malloc(N_ENTREVISTADOS*sizeof(char)); //Vetor onde fica armazenado o sexo dos entrevistados
+    int *op_entrevistados = (int*) malloc(N_ENTREVISTADOS*sizeof(int)); //Vetor onde fica armazenado se o entervistado gostou ou nao
+    //checando vetores
+    if(sexo == NULL || op_entrevistados == NULL){
+        printf("Erro de alocação de memoria\n");
+        exit(1);
+    } 
+        
+    for(i = 0; i<N_ENTREVISTADOS; i++){
+        //Pedindo para o entrevistado digitar seu sexo
+        printf("M = Masculino\nF = Feminino\n");
+        printf("Digite seu sexo(M/F): \n");
+        scanf(" %[^\n]", &sexo[i]);
+        sexo[i] = toupper(sexo[i]);
+        //Verificando se o entrevistado digitou uma opcao valida
+        if(sexo[i]!='M' && sexo[i] != 'F'){
+            printf("Digite somente M ou F!\n");
+            i--;
+        }
+        else {
+             //Perguntando se o entrevistado gostou ou nao do produto
+            printf("Digite sua opiniao sobre o produto\n1- gostou.\n0- Nao gostou.\n");
+            scanf("%i", &op);
+            if(op!= 0 && op!=1){
+                printf("Opcao invalida!\n");
+                i--;
+            }else op_entrevistados[i] = op;
+        }
+    } 
+    for (i=0;i<N_ENTREVISTADOS;i++){
+        //Adicionando contadores as variaveis
+        if(sexo[i]=='M'){
+            n_homem++;
+        }
+        else if(sexo[i]=='F'){
+            n_mulher++;
+        }
+        if (op_entrevistados[i]==1 && sexo[i]=='F'){
+            perc_f++;
+        }
+        else if (op_entrevistados[i]==0 && sexo[i]=='M'){
+            perc_m++;
+        }
+    }
+    
+    //Calculando os percentuais e em seguida exibindo
+    perc_f = n_mulher!=0 ? (perc_f*100)/n_mulher : 0;
+    perc_m = n_homem!=0 ? (perc_m*100)/n_homem : 0;
+    printf("Percentuais dos entrevistados:\n");
+    printf("Mulheres que gostaram: %.1f%%\nHomens que nao gostaram: %.1f%%", perc_f, perc_m);
+    
+    //liberando memoria
+    free(sexo);
+    free(op_entrevistados);
+
     return 0;
 }
