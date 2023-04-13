1f26c8eaf1e3b1c0dc874017f8b05dd28ee151b4
Caueh Oliveira <12563720+CauehCraft@users.noreply.github.com>
2023-04-13 15:04:48 -0300

    Update lista.h

--- listas_encadeadas/lista.h
+++ listas_encadeadas/lista.h
@@ -23,3 +23,15 @@ void lst_libera(Lista* l);
 
 // função que insere de forma ordenada um elemento na lista
 Lista* lst_insere_ordenada(Lista * l, int v);
+
+// função que retorna o número de nós da lista que possuem o campo info com valores maiores do que n
+int maiores(Lista* l, int n);
+
+// função que retorna o ponteiro para o último nó de uma lista encadeada.
+Lista* ultimo(Lista* l);
+
+// função que recebe duas listas encadeadas e retorne a lista resultante da concatenação das duas listas
+Lista* contatena(Lista* l1, Lista* l2);
+
+// função que retire da lista todas as ocorrências de n e retorna a lista resultante
+Lista* retira_n(Lista* l, int n);
